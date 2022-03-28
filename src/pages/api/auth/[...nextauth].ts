1185820a0b874eeef03a2d99213ffbbb5ec65751
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2022-03-28 00:16:29 -0300

    Setting payments with stripe

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,57 @@
+import { query as q} from 'faunadb';
+import { signIn } from 'next-auth/react';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+          }
+        }
+      }),
+  ],
+  secret: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+  callbacks: {
+    
+    async signIn( {user, account, profile} ) {
+      try {
+      const { email } = user;
+        await fauna.query(
+         q.If(
+           q.Not(
+             q.Exists(
+               q.Match(
+                  q.Index('user_by_email'),
+                    q.Casefold(user.email)
+               )
+             )
+           ),
+           q.Create(
+             q.Collection('users'),
+             { data: { email } }
+           ),
+           q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+                q.Casefold(user.email)
+            )
+           )
+         )
+        )
+        return true;
+
+      } catch(err) {
+        console.log(err);
+        return false;
+      }
+    }  
+  },
+})
